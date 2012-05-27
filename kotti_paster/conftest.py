06b69a1ca37834974ca753b26b4c92277c9e4ade
Tom Lazar <tomster@pyfidelity.com>
2012-05-27 02:10:39 +0200

    add some documentation

--- kotti_paster/conftest.py
+++ kotti_paster/conftest.py
@@ -7,11 +7,25 @@ from py.test import mark
 from mr.laforge import up, waitforports, shutdown
 
 paster = mark.paster
-
 home = pkg_resources.get_distribution('kotti_paster').location
 
 
 def pytest_funcarg__pasterdir(request):
+    """ Creates a temporary directory, changes into it and runs ``paster create`` in it
+    using the template name, project name and any additional parameters passed in via
+    the ``paster`` marker.
+
+    The return value is a tuple of the full path of the temporary directory, the full
+    path to the directory created by the temporary directory and the name of the project.
+
+    E.g. the following marker would call ``paster create -t foo bar --no-interactive``::
+
+        @paster('foo', 'bar', '--no-interactive')
+        def test_foo_bar(pasterdir):
+            assert 'foo.txt' in os.listdir(pasterdir)
+
+    The teardown method deletes the temporary directory again.
+    """
 
     def setup():
         directory = tempfile.mkdtemp()
@@ -34,7 +48,12 @@ def pytest_funcarg__pasterdir(request):
 
 
 def pytest_funcarg__application(request):
+    """ Assuming that ``pytest_funcarg__pasterdir`` has run and created a directory containing a ``buildout.cfg``
+    and a ``development.ini`` that configures an application on port 6543, runs that buildout
+    and starts the application via supervisor.
 
+    On teardown supervisord is shut down.
+    """
     def setup():
         tempdir, cwd, project = request._funcargs['pasterdir']
         cfg = open(os.path.join(cwd, 'supervisor.cfg'), 'w')
@@ -64,6 +83,12 @@ programs =
 
 
 def pytest_funcarg__pytest_runner(request):
+    """ Assuming that ``pytest_funcarg__pasterdir`` has run, creates and runs a testrunner for its egg.
+
+    Return value is an instance of ``subprocess.Popen`` running the tests.
+
+    Teardown terminates that process.
+    """
     def setup():
         # create a pytest runner via buildout
         tempdir, cwd, project = request._funcargs['pasterdir']
