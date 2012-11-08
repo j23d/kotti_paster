510d3c8ec9c864e731328fb8ab716d385d6ab2b0
Marco Scheidhuber <marco@jusid.de>
2012-11-08 10:44:03 +0100

    Prepare kotti_paster 0.1a3.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(name='kotti_paster',
-      version='0.1a3dev',
+      version='0.1a3',
       description="Paster AddOn Template for Kotti",
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
